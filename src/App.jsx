c8341a0ae8c4f5fdea5801ccaae7f3e196371912
Natalia Macias <natajenkins@hotmail.com>
2023-11-02 22:51:43 -0600

    summary card

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.scss";
+import DesktopPattern from "./assets/images/pattern-background-desktop.svg";
+import MobilePattern from "./assets/images/pattern-background-mobile.svg";
+import Illustration from "./assets/images/illustration-hero.svg";
+import MusicIcon from "./assets/images/icon-music.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <main className="container">
+            <div className="bg-curve">
+                <img src={DesktopPattern} className="desktop-pattern" alt="" />
+                <img src={MobilePattern} className="mobile-pattern" alt="" />
+            </div>
+            <div className="card">
+                <img src={Illustration} alt="" className="illustration" />
+                <div className="content">
+                    <h1 className="title"> Order Summary</h1>
+                    <p className="description">
+                        You can now listen to millions of songs, audiobooks, and
+                        podcasts on any device anywhere you like!
+                    </p>
+                    <div className="summary">
+                        <div className="left">
+                            <img src={MusicIcon} alt="" className="icon" />
+                            <div className="plan">
+                                <p className="label">Annual Plan</p>
+                                <p className="price">$59.99/year</p>
+                            </div>
+                        </div>
+                        <button className="action" onClick={() => null}>
+                            Change
+                        </button>
+                    </div>
+                    <div className="buttons">
+                        <button className="submit" onClick={() => null}>
+                            Proceed to Payment
+                        </button>
+                        <button className="cancel" onClick={() => null}>
+                            Cancel Order
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
 }
 
-export default App
+export default App;
